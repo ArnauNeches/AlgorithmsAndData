57872fa9998dd31a4c63e2e5401867cdead1f27e
Arnau Neches Vilà <arnnecvil@us.es>
2023-07-30 23:04:41 +0200

    Beginning of chapter 2

---
+++ Chapter2/AlgAnalisis22.py
@@ -0,0 +1,23 @@
+#I will check the time of execution of some function using the time module
+
+import time
+
+def sum_of_n(n):
+    s = time.time()
+    res = 0
+    for i in range(1, n+1):
+        res += i
+    t = time.time()-s
+    print(f"The sum of the {n} first numbers is {res} and it took {t:10.15f} seconds")
+
+#Now using a formula we'll see that it takes less
+
+def sum_of_n2(n):
+    s = time.time()
+    res = (n*(n+1))/2
+    t = time.time()-s
+    print(f"The sum of the {n} first numbers is {res} and it took {t:10.15f} seconds")
+
+sum_of_n(100000000)
+sum_of_n2(100000000)
+
