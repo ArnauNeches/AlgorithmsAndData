987b74a8813013cec7763a1156b2d65675888ffd
Arnau Neches Vilà <arnnecvil@us.es>
2023-07-30 23:32:16 +0200

    Test

--- Chapter2/AlgAnalisis22.py
+++ Chapter2/AlgAnalisis22.py
@@ -8,7 +8,7 @@ def sum_of_n(n):
     for i in range(1, n+1):
         res += i
     t = time.time()-s
-    print(f"The sum of the {n} first numbers is {res} and it took {t:10.15f} seconds")
+    print(f"The sum of the {n} first numbers is {res} and it took {t:.15f} seconds")
 
 #Now using a formula we'll see that it takes less
 
@@ -16,8 +16,8 @@ def sum_of_n2(n):
     s = time.time()
     res = (n*(n+1))/2
     t = time.time()-s
-    print(f"The sum of the {n} first numbers is {res} and it took {t:10.15f} seconds")
+    print(f"The sum of the {n} first numbers is {res} and it took {t:.15f} seconds")
 
-sum_of_n(100000000)
-sum_of_n2(100000000)
+sum_of_n(10000000)
+sum_of_n2(10000000)
 
