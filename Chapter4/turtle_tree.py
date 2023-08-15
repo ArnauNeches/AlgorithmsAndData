6602b143283ec56e9c9ec9644d68d6fd6a6ee0d5
Arnau Neches Vilà <arnichi2103@gmail.com>
2023-08-15 22:45:57 +0200

    recursion

--- Chapter4/turtle_tree.py
+++ Chapter4/turtle_tree.py
@@ -1,16 +1,21 @@
 import turtle
+from random import randint
+from math import log
 
 my_turtle = turtle.Turtle()
 my_win = turtle.Screen()
 
 def tree(branch_len, t):
     if branch_len > 5:
+        num = randint(15, 45)
+        num2 = randint(12, 17)
+        t.pensize(log(branch_len, 1.95))
         t.forward(branch_len)
-        t.right(30)
-        tree(branch_len - 15, t)
-        t.left(60)
-        tree(branch_len - 15, t)
-        t.right(30)
+        t.right(num)
+        tree(branch_len - num2, t)
+        t.left(num*2)
+        tree(branch_len - num2, t)
+        t.right(num)
         t.backward(branch_len)
 
 if __name__ == "__main__":
@@ -19,5 +24,5 @@ if __name__ == "__main__":
     my_turtle.backward(100)
     my_turtle.down()
     my_turtle.color("green")
-    tree(75, my_turtle)
+    tree(100, my_turtle)
     my_win.exitonclick()
