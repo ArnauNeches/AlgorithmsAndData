ba0d28feeb916d542f7580dcb584d78bffeb70bd
Arnau Neches Vilà <arnichi2103@gmail.com>
2023-08-24 21:41:12 +0200

    sort

---
+++ Chapter5/Sorting/selection_sort.py
@@ -0,0 +1,8 @@
+def selection_sort(a_list):
+    for fill_slot in range(len(a_list) - 1, 0, -1):
+        pos_of_max = 0
+        for location in range(1, fill_slot + 1):
+            if a_list[location] > a_list[pos_of_max]:
+                pos_of_max = location
+        
+        a_list[fill_slot], a_list[pos_of_max] = a_list[pos_of_max], a_list[fill_slot]
