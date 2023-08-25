639558ca163716593cdd17cfb97a41fdd797336b
Arnau Neches Vilà <arnichi2103@gmail.com>
2023-08-25 10:57:26 +0200

    sorting

--- Chapter5/Sorting/insertion_sort.py
+++ Chapter5/Sorting/insertion_sort.py
@@ -1,2 +1,10 @@
 def insertion_sort(a_list):
-    pass
+    for index in range(1, len(a_list)):
+        current_value = a_list[index]
+        position = index
+
+        while position > 0 and a_list[position - 1] > current_value:
+            a_list[position] = a_list[position - 1]
+            position = position - 1
+            
+        a_list[position] = current_value
